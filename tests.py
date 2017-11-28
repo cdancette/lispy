2f8ecb083e1dc7af46ee0c77b1d52837a1671b4f
Corentin <corentin.dancette@student.ecp.fr>
2017-11-28 16:59:18 +0100

    add tests

--- tests.py
+++ tests.py
@@ -38,11 +38,52 @@ class Test(TestCase):
             self.assertEqual(eval(parse(expr), env), expected_val)
 
 
-        # test keywords
+        # test basic operations
 
+        test_expr("3", 3)
+        test_expr("3.14", 3.14)
+        test_expr("#f", False)
+        test_expr("#t", True)
+
+        test_expr("(+ 1 2)", 3)
+        test_expr("(- 2.5 1.5)", 1.0)
+        test_expr("(/ 3 2)", 1.5)
+        test_expr("(// 3 2)", 1)
+
+
+        test_expr("(if #t 3 2)", 3)
+        test_expr("(if #f 3 2)", 2)
+        test_expr("(if (> 3 2) 3 2)", 3)
+
+        test_expr("(begin 2 3 5)", 5)
+
+        test_expr("(quote (define x 2))", ["define",  "x", 2])
+
+        test_expr("(list 1 2 3)", [1, 2, 3])
+        test_expr("(list #f 2 3.14)", [False, 2, 3.14])
+        test_expr("(head (list 1 2))", 1)
+        test_expr("(tail (list 1 2 3))", [2, 3])
+        test_expr("(tail (list 1))", [])
+        test_expr("(empty? (list))", True)
+        test_expr("(empty? (list 1))", False)
+
+        test_expr("(quote (list 1))", ['list', 1])
+
+        test_expr("(begin (define x 3) x)", 3)
+        test_expr("(begin (define x 3) (set! x 5) x)", 5)
 
         test_expr("(defun (f x) (+ x 2))", None)
         test_expr("(begin (defun (f x) (+ x 2)) (f 3))", 5)
+
+        # test recursive factorial
+        test_expr("(begin (defun (fact n) (if (<= n 1) 1 (* n (fact (- n 1))))) (fact 10))", 3628800)
+
+        # test closure
+        test_expr("(begin "
+                  "(define add "
+                  "     (lambda (x) (lambda (y) (+ x y))))"
+                  "(define add10 (add 10)) (add10 5)"
+                  ")", 15)
 
     def test_eval(self):
         env = create_env()
